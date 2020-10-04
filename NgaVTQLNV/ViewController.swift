fac064a2725bae5e297d115e3b82d562e939cbbb
Ngavt <ngavt@ttc-solutions.com>
2020-10-04 22:26:55 +0700

    Initial Commit

---
+++ NgaVTQLNV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NgaVTQLNV
+//
+//  Created by Admin on 10/4/20.
+//  Copyright © 2020 NgaVu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
